aa5348680c97045baffa36cde74ba40374b3886a
Tatyana Shashkova <tatyana.shashkova@inworld.ai>
2023-04-05 14:23:28 -0700

    fix: Change jest config extension

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  coveragePathIgnorePatterns: ['node_modules', 'proto'],
-  preset: 'ts-jest',
-  moduleNameMapper: {
-    '@proto/(.*)': '<rootDir>/proto/$1',
-    '@root/(.*)': '<rootDir>/$1',
-  },
-  modulePathIgnorePatterns: ['__tests__/helpers.ts'],
-};
