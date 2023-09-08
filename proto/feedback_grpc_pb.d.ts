e00cedc9e5854b3cb08f7107e9384a7039925070
Tatyana Shashkova <tatyana.shashkova@inworld.ai>
2023-09-08 13:48:54 -0700

    feat: Add feedback grpc service

---
+++ proto/feedback_grpc_pb.d.ts
@@ -0,0 +1,45 @@
+// package: ai.inworld.engine.v1
+// file: ai/inworld/engine/v1/feedback.proto
+
+/* tslint:disable */
+/* eslint-disable */
+
+import * as grpc from "@grpc/grpc-js";
+import * as ai_inworld_engine_v1_feedback_pb from "./feedback_pb";
+
+
+
+import * as google_protobuf_empty_pb from "google-protobuf/google/protobuf/empty_pb";
+
+interface IFeedbackService extends grpc.ServiceDefinition<grpc.UntypedServiceImplementation> {
+    createInteractionFeedback: IFeedbackService_ICreateInteractionFeedback;
+}
+
+interface IFeedbackService_ICreateInteractionFeedback extends grpc.MethodDefinition<ai_inworld_engine_v1_feedback_pb.CreateInteractionFeedbackRequest, google_protobuf_empty_pb.Empty> {
+    path: "/ai.inworld.engine.v1.Feedback/CreateInteractionFeedback";
+    requestStream: false;
+    responseStream: false;
+    requestSerialize: grpc.serialize<ai_inworld_engine_v1_feedback_pb.CreateInteractionFeedbackRequest>;
+    requestDeserialize: grpc.deserialize<ai_inworld_engine_v1_feedback_pb.CreateInteractionFeedbackRequest>;
+    responseSerialize: grpc.serialize<google_protobuf_empty_pb.Empty>;
+    responseDeserialize: grpc.deserialize<google_protobuf_empty_pb.Empty>;
+}
+
+export const FeedbackService: IFeedbackService;
+
+export interface IFeedbackServer {
+    createInteractionFeedback: grpc.handleUnaryCall<ai_inworld_engine_v1_feedback_pb.CreateInteractionFeedbackRequest, google_protobuf_empty_pb.Empty>;
+}
+
+export interface IFeedbackClient {
+    createInteractionFeedback(request: ai_inworld_engine_v1_feedback_pb.CreateInteractionFeedbackRequest, callback: (error: grpc.ServiceError | null, response: google_protobuf_empty_pb.Empty) => void): grpc.ClientUnaryCall;
+    createInteractionFeedback(request: ai_inworld_engine_v1_feedback_pb.CreateInteractionFeedbackRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: google_protobuf_empty_pb.Empty) => void): grpc.ClientUnaryCall;
+    createInteractionFeedback(request: ai_inworld_engine_v1_feedback_pb.CreateInteractionFeedbackRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: google_protobuf_empty_pb.Empty) => void): grpc.ClientUnaryCall;
+}
+
+export class FeedbackClient extends grpc.Client implements IFeedbackClient {
+    constructor(address: string, credentials: grpc.ChannelCredentials, options?: object);
+    public createInteractionFeedback(request: ai_inworld_engine_v1_feedback_pb.CreateInteractionFeedbackRequest, callback: (error: grpc.ServiceError | null, response: google_protobuf_empty_pb.Empty) => void): grpc.ClientUnaryCall;
+    public createInteractionFeedback(request: ai_inworld_engine_v1_feedback_pb.CreateInteractionFeedbackRequest, metadata: grpc.Metadata, callback: (error: grpc.ServiceError | null, response: google_protobuf_empty_pb.Empty) => void): grpc.ClientUnaryCall;
+    public createInteractionFeedback(request: ai_inworld_engine_v1_feedback_pb.CreateInteractionFeedbackRequest, metadata: grpc.Metadata, options: Partial<grpc.CallOptions>, callback: (error: grpc.ServiceError | null, response: google_protobuf_empty_pb.Empty) => void): grpc.ClientUnaryCall;
+}
---
+++ proto/feedback_grpc_pb.js
@@ -0,0 +1,53 @@
+// GENERATED CODE -- DO NOT EDIT!
+
+'use strict';
+var grpc = require('@grpc/grpc-js');
+var ai_inworld_engine_v1_feedback_pb = require('./feedback_pb.js');
+
+
+
+
+var google_protobuf_empty_pb = require('google-protobuf/google/protobuf/empty_pb.js');
+
+function serialize_ai_inworld_engine_v1_CreateInteractionFeedbackRequest(arg) {
+  if (!(arg instanceof ai_inworld_engine_v1_feedback_pb.CreateInteractionFeedbackRequest)) {
+    throw new Error('Expected argument of type ai.inworld.engine.v1.CreateInteractionFeedbackRequest');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_ai_inworld_engine_v1_CreateInteractionFeedbackRequest(buffer_arg) {
+  return ai_inworld_engine_v1_feedback_pb.CreateInteractionFeedbackRequest.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+function serialize_google_protobuf_Empty(arg) {
+  if (!(arg instanceof google_protobuf_empty_pb.Empty)) {
+    throw new Error('Expected argument of type google.protobuf.Empty');
+  }
+  return Buffer.from(arg.serializeBinary());
+}
+
+function deserialize_google_protobuf_Empty(buffer_arg) {
+  return google_protobuf_empty_pb.Empty.deserializeBinary(new Uint8Array(buffer_arg));
+}
+
+
+// Contains methods to post feedback
+var FeedbackService = exports.FeedbackService = {
+  // (-- api-linter: core::0133::response-message-name=disabled
+//     aip.dev/not-precedent: Always returns empty result. --)
+// Creates a feedback for session message from agent
+createInteractionFeedback: {
+    path: '/ai.inworld.engine.v1.Feedback/CreateInteractionFeedback',
+    requestStream: false,
+    responseStream: false,
+    requestType: ai_inworld_engine_v1_feedback_pb.CreateInteractionFeedbackRequest,
+    responseType: google_protobuf_empty_pb.Empty,
+    requestSerialize: serialize_ai_inworld_engine_v1_CreateInteractionFeedbackRequest,
+    requestDeserialize: deserialize_ai_inworld_engine_v1_CreateInteractionFeedbackRequest,
+    responseSerialize: serialize_google_protobuf_Empty,
+    responseDeserialize: deserialize_google_protobuf_Empty,
+  },
+};
+
+exports.FeedbackClient = grpc.makeGenericClientConstructor(FeedbackService);
