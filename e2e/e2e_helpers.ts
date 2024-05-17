3227e06c039fd5921524132c518ac4bc9b9170f2
russinworld <169074532+russinworld@users.noreply.github.com>
2024-05-17 15:34:42 -0400

    E2E Helper Functions and Test GHA Workflows (#149)
    * sendText helper function
    
    * sendAudio working
    
    * sendAudio fixes
    
    * Basic daily run workflow test
    
    * sendText test
    
    * npm install daily run
    
    * checkout source code
    
    * removed working dir
    
    * Commented out dotenv
    
    * fixes
    
    * Added secrets
    
    * fix
    
    * Created demand run
    
    * Fix
    
    * Fixes
    
    * test fix
    
    * fixes
    
    * fix
    
    * Setup node
    
    * Updated text spec
    
    * removed dotenv
    
    * Set types
    
    * Added npm install inworld to build-pr workflow
    
    * Refactored
    
    * Added test:unit and test:e2e script
    
    * Updated build workflow

---
+++ e2e/e2e_helpers.ts
@@ -0,0 +1,126 @@
+import {
+  InworldClient,
+  InworldPacket,
+  ServiceError,
+  status,
+} from '@inworld/nodejs-sdk';
+import * as fs from 'fs';
+
+export async function sendText(
+  apikey: [string, string],
+  username: string,
+  scene: string,
+  message: string,
+): Promise<[string, string]> {
+  let output: [string, string] = ['', ''];
+
+  return new Promise<[string, string]>(async (resolve, reject) => {
+    const client = new InworldClient()
+      .setApiKey({
+        key: apikey[0],
+        secret: apikey[1],
+      })
+      .setUser({ fullName: username })
+      .setConfiguration({
+        capabilities: { emotions: true },
+      })
+      .setScene(scene)
+      .setOnError((err: any) => {
+        switch (err.code) {
+          case status.ABORTED:
+          case status.CANCELLED:
+            break;
+          default:
+            reject(err);
+            break;
+        }
+      })
+      .setOnMessage((packet: InworldPacket) => {
+        // TEXT
+        if (packet.isText()) {
+          output[0] += packet.text.text + '\n';
+        }
+
+        // EMOTION
+        if (packet.isEmotion()) {
+          output[1] += packet.emotions.behavior.code + '\n';
+          output[1] += packet.emotions.strength.code + '\n';
+        }
+
+        // INTERACTION_END
+        if (packet.isInteractionEnd()) {
+          connection.close();
+          resolve(output);
+        }
+      });
+
+    const connection = client.build();
+
+    await connection.sendText(message);
+  });
+}
+
+export async function sendAudio(
+  apikey: [string, string],
+  username: string,
+  scene: string,
+  audio: string,
+): Promise<string> {
+  let output: string = '';
+  const timeout = 200;
+  const highWaterMark = 1024 * 5;
+
+  return new Promise<string>(async (resolve, reject) => {
+    let i = 0;
+
+    const audioStream = fs.createReadStream(audio, { highWaterMark });
+    const sendChunk = (chunk: string) => {
+      setTimeout(() => {
+        connection.sendAudio(chunk);
+      }, timeout * i);
+      i++;
+    };
+
+    const connection = new InworldClient()
+      .setApiKey({
+        key: apikey[0],
+        secret: apikey[1],
+      })
+      .setScene(scene)
+      .setUser({ fullName: username })
+      .setOnError((err: ServiceError) => {
+        reject(err);
+      })
+      .setOnMessage((packet: InworldPacket) => {
+        if (packet.isText() && packet.routing.source.isPlayer) {
+          if (packet.text.final) {
+            connection.sendAudioSessionEnd();
+          }
+        }
+
+        if (packet.isText() && packet.routing.source.isCharacter) {
+          output += packet.text.text + '\n';
+        }
+
+        if (packet.isInteractionEnd()) {
+          connection.close();
+          resolve(output);
+        }
+      })
+      .build();
+
+    await connection.sendAudioSessionStart();
+
+    audioStream.on('data', sendChunk).on('end', async () => {
+      audioStream.close();
+
+      const silenceStream = fs.createReadStream('e2e/audio/silence.wav', {
+        highWaterMark,
+      });
+
+      silenceStream
+        .on('data', sendChunk)
+        .on('end', () => silenceStream.close());
+    });
+  });
+}
