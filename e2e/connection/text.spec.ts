1409c78b2f64224ac97daab81aa5eb990921d525
russinworld <russ@withinworld.ai>
2024-06-04 14:11:21 -0400

    test fixes

--- e2e/connection/text.spec.ts
+++ e2e/connection/text.spec.ts
@@ -14,9 +14,9 @@ let scene: string = process.env.INWORLD_E2E_SCENE_HOUSE!;
 test('Should return a response', async () => {
   const result = await sendText(key, name, scene, 'Hi there!');
   expect(result[0]).not.toBe('');
-  await allure.parentSuite('Tests for web interface');
-  await allure.suite('Tests for essential features');
-  await allure.subSuite('Tests for authentication');
+  await allure.suite('Node.js SDK');
+  await allure.feature('Connection');
+  await allure.label('component', 'Text');
   await allure.description(
     'This test attempts to log into the website using a login and a password. Fails if any error happens.\n\nNote that this test does not test 2-Factor Authentication.',
   );
