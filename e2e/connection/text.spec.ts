c9edb2b005c6fdfb13ef910d80b0f8e1db6593ef
russinworld <russ@withinworld.ai>
2024-06-04 16:44:46 -0400

    update metadata

--- e2e/connection/text.spec.ts
+++ e2e/connection/text.spec.ts
@@ -11,7 +11,7 @@ let key: [string, string] = [
 let name: string = 'Tester';
 let scene: string = process.env.INWORLD_E2E_SCENE_HOUSE!;
 
-test('Should return a response', async () => {
+test('[Text] Should return a response', async () => {
   await allure.allureId('925');
   await allure.suite('Node.js SDK');
   await allure.feature('Connection');
@@ -19,10 +19,6 @@ test('Should return a response', async () => {
   await allure.description(
     'This test confirms that a response is recieved when sending text to an NPC',
   );
-  await allure.displayName(
-    'Node js > Connection > Text: Should return a response',
-  );
-
   const result = await sendText(key, name, scene, 'Hi there!');
   expect(result[0]).not.toBe('');
 }, 10000);
