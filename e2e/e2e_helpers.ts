1e792f2eabfe97eeabd94d480df82d17ffa80dc4
russinworld <russ@withinworld.ai>
2024-06-21 11:42:05 -0400

    changeScene method

--- e2e/e2e_helpers.ts
+++ e2e/e2e_helpers.ts
@@ -168,3 +168,60 @@ export async function getPacketsNewChat(
     resolve(packets);
   });
 }
+
+export async function changeSceneSendText(
+  apikey: [string, string],
+  username: string,
+  npc: string,
+  message: string,
+  scene: string,
+): Promise<[string, string]> {
+  let output: [string, string] = ['', ''];
+
+  return new Promise<[string, string]>(async (resolve, reject) => {
+    const client = new InworldClient()
+      .setApiKey({
+        key: apikey[0],
+        secret: apikey[1],
+      })
+      .setUser({ fullName: username })
+      .setConfiguration({
+        capabilities: { emotions: true },
+      })
+      .setScene(npc)
+      .setOnError((err: InworldError) => {
+        switch (err.code) {
+          case status.ABORTED:
+          case status.CANCELLED:
+            break;
+          default:
+            connection.close();
+            reject(err);
+            break;
+        }
+      })
+      .setOnMessage((packet: InworldPacket) => {
+        // TEXT
+        if (packet.isText()) {
+          output[0] += packet.text.text + '\n';
+        }
+
+        // EMOTION
+        if (packet.isEmotion()) {
+          output[1] += packet.emotions.behavior.code + '\n';
+          output[1] += packet.emotions.strength.code + '\n';
+        }
+
+        // INTERACTION_END
+        if (packet.isInteractionEnd()) {
+          connection.close();
+          resolve(output);
+        }
+      });
+
+    const connection = client.build();
+
+    await connection.changeScene(scene);
+    await connection.sendText(message);
+  });
+}
