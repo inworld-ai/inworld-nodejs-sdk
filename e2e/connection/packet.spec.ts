f31c75c1e2ca33f39475623b25aba34dd7a97315
russinworld <russ@withinworld.ai>
2024-06-25 15:00:43 -0400

    Packet spec

--- e2e/connection/packet.spec.ts
+++ e2e/connection/packet.spec.ts
@@ -1,42 +1,39 @@
-import 'dotenv/config';
+import * as allure from 'allure-js-commons';
 
-// import * as allure from 'allure-js-commons';
-import { openConnectionManuallySendText } from '../e2e_helpers';
+import {
+  openConnectionManually,
+  openConnectionManuallySendText,
+} from '../e2e_helpers';
 
 let key: [string, string] = [
   process.env.INWORLD_E2E_KEY!,
   process.env.INWORLD_E2E_SECRET!,
 ];
 let name: string = 'Tester';
-let scene: string = process.env.INWORLD_E2E_CHARACTER_TEXT!;
+let npc: string = process.env.INWORLD_E2E_CHARACTER_TEXT!;
 
-// test('[Packet] Packet properties are correct for new connection with no text sent', async () => {
-//   await allure.allureId('1255');
-//   await allure.suite('Node.js SDK');
-//   await allure.feature('Connection');
-//   await allure.story('Packet');
-//   await allure.description(
-//     'This test confirms that all packet properties for a new connection with no text sent are correct',
-//   );
+test('[Packet] Packet properties are correct for new connection with no text sent', async () => {
+  await allure.allureId('1255');
+  await allure.suite('Node.js SDK');
+  await allure.feature('Connection');
+  await allure.story('Packet');
+  await allure.description(
+    'This test confirms that all packet properties for a new connection with no text sent are correct',
+  );
 
-//   const connection = await openConnectionManually(key, name, scene);
-//   connection.close();
-// }, 10000);
+  const connection = await openConnectionManually(key, name, npc);
+  connection.close();
+}, 10000);
 
 test('[Packet] Packet properties are correct for new connection with text sent', async () => {
   // await allure.allureId('1255');
-  // await allure.suite('Node.js SDK');
-  // await allure.feature('Connection');
-  // await allure.story('Packet');
-  // await allure.description(
-  //   'This test confirms that all packet properties for a new connection with no text sent are correct',
-  // );
-
-  const connection = await openConnectionManuallySendText(
-    key,
-    name,
-    scene,
-    'Hi',
+  await allure.suite('Node.js SDK');
+  await allure.feature('Connection');
+  await allure.story('Packet');
+  await allure.description(
+    'This test confirms that all packet properties for a new connection with text sent are correct',
   );
+
+  const connection = await openConnectionManuallySendText(key, name, npc, 'Hi');
   connection.close();
 }, 10000);
