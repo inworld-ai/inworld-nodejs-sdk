077e2df306287fa4da21e4802144bb7f69df2067
russinworld <169074532+russinworld@users.noreply.github.com>
2024-07-18 15:43:00 -0400

    Test 90 add test to cover trigger interaction (#236)
    * sendTrigger method and trigger spec
    
    * Added allureid
    
    * Removed spaces

--- e2e/connection/packet.spec.ts
+++ e2e/connection/packet.spec.ts
@@ -9,16 +9,17 @@ let key: [string, string] = [
 let name: string = 'Tester';
 let npc: string = process.env.INWORLD_E2E_CHARACTER_TEXT!;
 let npc2: string = process.env.INWORLD_E2E_CHARACTER_NARRATOR!;
+let npc3: string = process.env.INWORLD_E2E_CHARACTER_VERB!;
 
-const config2 = {
-  capabilities: { emotions: true, narratedActions: true },
+const config = {
+  capabilities: { emotions: true },
   connection: {
     autoReconnect: false,
   },
 };
 
-const config = {
-  capabilities: { emotions: true },
+const config2 = {
+  capabilities: { emotions: true, narratedActions: true },
   connection: {
     autoReconnect: false,
   },
@@ -79,5 +80,19 @@ test('[Packet] Packet properties are correct for new connection with narrated ac
     '{character}s neighbour Paul was murdered in his apartment. {player}, who is a detective, responsible for investigating the case, meets {character} near entrance of their apartment.',
   );
   await connection.sendText('Hello');
+  connection.close();
+}, 10000);
+
+test('[Packet] Packet properties are correct for new connection with trigger sent', async () => {
+  await allure.allureId('1682');
+  await allure.suite('Node.js SDK');
+  await allure.feature('Connection');
+  await allure.story('Packet');
+  await allure.description(
+    'This test confirms that all packet properties for a new connection with trigger sent are correct',
+  );
+
+  const connection = await openConnectionManually(key, name, npc3, config);
+  await connection.sendTrigger('greeting');
   connection.close();
 }, 10000);
